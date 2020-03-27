b0c8eb5d471f64446b96c1110d7502dbc788aeb2
valdei777 <56629734+valdei777@users.noreply.github.com>
2020-03-28 02:59:11 +0300

    Update lesson_9.sql

--- lesson_9.sql
+++ lesson_9.sql
@@ -1,7 +1,24 @@
 -- 1. В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных.
 Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции. 
 
+START TRANSACTION;
+  INSERT INTO sample.users SELECT * FROM shop.users WHERE id = 1;
+  DELETE FROM shop.users WHERE id = 1 LIMIT 1;
+COMMIT;
+
 
 
 -- 2. Создайте представление, которое выводит название name товарной позиции из таблицы products
 и соответствующее название каталога name из таблицы catalogs. 
+
+
+CREATE OR REPLACE VIEW products_catalogs AS
+SELECT
+  p.name AS product,
+  c.name_category AS catalog
+FROM
+  product AS p
+JOIN
+  category AS c
+ON
+  p.category_id = c.id;
